09f20355f3312ddd7604398f18757194186b3cf1
Anlimah <y.m.ratty7@gmail.com>
2022-05-29 11:46:03 +0000

    Updating payment response

--- purchase/purchase_confirm.php
+++ purchase/purchase_confirm.php
@@ -9,7 +9,7 @@ use Src\Controller\PaymentGateway;
 if (isset($_GET['status']) && !empty($_GET['status']) && $_GET['status'] == 'cancelled') {
     echo 'Payment processing was cancelled';
     //PaymentGateway::destroyAllSessions();
-    header('Location: purchase_step1.php?status=cancelled');
+    //header('Location: purchase_step1.php?status=cancelled');
 } elseif (isset($_GET['status']) && !empty($_GET['status']) && $_GET['status'] == 'successful') {
     $transRef = $_GET['tx_ref'];
     $transID = $_GET['transaction_id'];
