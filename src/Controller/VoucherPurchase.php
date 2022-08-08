79e219ac8fb22482acd6daff53b17635d6872b0e
Anlimah <y.m.ratty7@gmail.com>
2022-08-08 12:16:34 +0000

    Login details generation

---
+++ src/Controller/VoucherPurchase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Src\Controller;
+
+require_once('../bootstrap.php');
+
+use Twilio\Rest\Client;
+
+use Src\System\DatabaseMethods;
+
+class VoucherPurchase extends DatabaseMethods
+{
+    public function verifyEmailAddress($email, $code)
+    {
+        $sql = "SELECT `id` FROM `verify_email_address` WHERE `email_address`=:e AND `code`=:c";
+        return $this->getID($sql, array(':e' => $email, ':c' => $code));
+    }
+
+    public function verifyPhoneNumber($number, $code)
+    {
+        $sql = "SELECT `id` FROM `verify_phone_number` WHERE `phone_number`=:p AND `code`=:c";
+        return $this->getID($sql, array(':p' => $number, ':c' => $code));
+    }
+
+    private function genPin(int $length_pin = 9)
+    {
+        $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        return substr(str_shuffle($str_result), 0, $length_pin);
+    }
+
+    private function genAppNumber(int $type, int $year)
+    {
+        $user_code = $this->genCode(5);
+        $app_number = 'RMU-' . (($type * 10000000) + ($year * 100000) + $user_code);
+        return $app_number;
+    }
+
+    public function genLoginDetails($app_number, int $type, int $year)
+    {
+        $app_num = '';
+        while (true) {
+            $this->genAppNumber($type, $year);
+            $sql = "SELECT `id` FROM `applicants_login` WHERE `app_number`=:p";
+            if (!$this->getID($sql, array(':p' => $app_number))) {
+                break;
+            }
+        }
+        return array(
+            'app_number' => $this->genAppNumber($type, $year),
+            'pin_number' => $this->genPin()
+        );
+    }
+}
