7cc33be8bac510642a2a738288c73a235eff1709
Anlimah <y.m.ratty7@gmail.com>
2022-10-06 14:25:47 +0000

    Orchard payment gateway integration update 18

--- src/Controller/OrchardPaymentController.php
+++ src/Controller/OrchardPaymentController.php
@@ -42,8 +42,8 @@ if (isset($_SESSION['step1Done']) && isset($_SESSION['step2Done']) && isset($_SE
         $request = 'POST';
 
         $pay = new OrchardPaymentGateway($secretKey, $payUrl, $request, $payload);
-        $response = json_decode($pay->initiatePayment());/**/
-        echo $response->resp_code;
+        $response = $pay->initiatePayment();/**/
+        echo $response;
         /*if ($response["resp_code"] == "015") {
             //$_SESSION['processing'] = true;
             header("Location: " . $callback_url);
