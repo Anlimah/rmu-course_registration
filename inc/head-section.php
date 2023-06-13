4482e14848df2715d3b2465cb396e99ccbc5d49f
Anlimah <y.m.ratty7@gmail.com>
2023-06-13 20:20:52 +0000

    Added meta properties 3

--- inc/head-section.php
+++ inc/head-section.php
@@ -32,8 +32,8 @@ $user_id = $_SESSION['ghApplicant'];
     <meta property="og:image:width" content="400">
     <meta property="og:image:height" content="75">
     <meta property="og:description" content="Regional Maritime University (RMU) offers a comprehensive range of diploma and degree programs in Marine Engineering, Nautical Science, Electrical Engineering, Mechanical Engineering, Computer Science, Computer Engineering, Information Technology, Logistics, Port and Shipping Management, and other short courses. Explore our programs and gain expertise in the maritime industry. Join us and unlock your potential in the exciting world of maritime education.">
-    <link href="../assets/img/rmu-logo.png" rel="icon">
-    <link href="../assets/img/rmu-logo.png" rel="apple-touch-icon">
+    <link href="../assets/images/rmu-logo.png" rel="icon">
+    <link href="../assets/images/rmu-logo.png" rel="apple-touch-icon">
     <link rel="stylesheet" href="../../assets/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
