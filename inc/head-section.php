89da80de174c13b27c8de59309c3705e13439efe
Anlimah <y.m.ratty7@gmail.com>
2023-06-13 20:17:13 +0000

    Added meta properties

--- inc/head-section.php
+++ inc/head-section.php
@@ -25,6 +25,15 @@ $user_id = $_SESSION['ghApplicant'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <meta name="Keywords" content="RMU, University, Apply, Forms, School, Institution">
+    <meta name="Description" content="The Regional Maritime University (RMU), Accra, Ghana, is an international tertiary institution. The overall objective for the establishment of RMU is to promote regional co-operation in the maritime industry focusing on the training to ensure the sustained growth and development of the industry.">
+    <meta property="og:image" content="https://rmu.edu.gh/wp-content/uploads/2019/09/rmulogo-exp-3-400x75.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="400">
+    <meta property="og:image:height" content="75">
+    <meta property="og:description" content="Regional Maritime University (RMU) offers a comprehensive range of diploma and degree programs in Marine Engineering, Nautical Science, Electrical Engineering, Mechanical Engineering, Computer Science, Computer Engineering, Information Technology, Logistics, Port and Shipping Management, and other short courses. Explore our programs and gain expertise in the maritime industry. Join us and unlock your potential in the exciting world of maritime education.">
+    <link href="assets/img/rmu-logo.png" rel="icon">
+    <link href="assets/img/rmu-logo.png" rel="apple-touch-icon">
     <link rel="stylesheet" href="../../assets/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
